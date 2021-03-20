f13b5c6cf6dbcb98198fe48f06fcc6e42b9c3d6b
shima159753 <shimazakib13@gmail.com>
2021-03-20 14:40:59 +0900

    db修正

--- db/migrate/20210317072849_add_user_ref_and_room_ref_to_messages.rb
+++ db/migrate/20210317072849_add_user_ref_and_room_ref_to_messages.rb
@@ -1,6 +1,6 @@
 class AddUserRefAndRoomRefToMessages < ActiveRecord::Migration[6.0]
   def change
-    #remove_reference :messages, :user, foreign_key: true
-    #remove_reference :messages, :room, foreign_key: true
+    add_reference :messages, :user, foreign_key: true
+    add_reference :messages, :room, foreign_key: true
   end
 end
